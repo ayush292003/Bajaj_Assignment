d14655717e0032a4ea8ba85e44f6c74b8a6d20c6
Ayush Singh <74094464+ayush292003@users.noreply.github.com>
2023-05-19 19:10:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data.employees);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!filter) {
+      setFilteredEmployees(employees);
+    } else {
+      const filtered = employees.filter((employee) => {
+        const name = employee.name ? employee.name.toLowerCase() : "";
+        const designation = employee.designation
+          ? employee.designation.toLowerCase()
+          : "";
+        const skills = employee.skills
+          ? employee.skills.map((skill) => skill.toLowerCase())
+          : [];
+
+        return (
+          name.includes(filter.toLowerCase()) ||
+          designation.includes(filter.toLowerCase()) ||
+          skills.some((skill) => skill.includes(filter.toLowerCase()))
+        );
+      });
+      setFilteredEmployees(filtered);
+    }
+  }, [employees, filter]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Employee Filter</h1>
+      <input
+        type="text"
+        value={filter}
+        onChange={handleFilterChange}
+        placeholder="Filter by name/designation/skills"
+      />
+      <ul>
+        {filteredEmployees.map((employee) => (
+          <li key={employee.id}>
+            <h2>{employee.name}</h2>
+            <p>Designation: {employee.designation}</p>
+            <p>Skills: {employee.skills.join(", ")}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
